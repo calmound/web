738ccbf2e15b2d4973b04319fe7945d2dd7cab9a
calmound <245708505@qq.com>
2014-11-20 15:02:02 +0800

    Javascript事件

---
+++ javascript/事件/加载事件（onload）.js
@@ -0,0 +1,14 @@
+//实现加载页面时，弹出对话框“加载中，请稍等…”。
+<!DOCTYPE HTML>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title> 加载事件 </title>
+<script type="text/javascript">
+  function message(){
+    alert("加载中，请稍等…"); }
+</script>    
+</head>
+<body onload="message()">
+  欢迎学习JavaScript。
+</body>
+</html>
