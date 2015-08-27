bd7d9b725cd3b64078bd00e5fd43c8f4d41a8c93
calmound <245708505@qq.com>
2015-08-27 10:08:43 +0800

    增加js实例，重新制定了文件路径

---
+++ web前端/javascript/基础/事件/内容选中事件（onselect）.js
@@ -0,0 +1,17 @@
+﻿//当选中个人简介文本框中文字时，触发onselect事件，并弹出对话框。
+<!DOCTYPE HTML>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title> 内容选中事件 </title>
+<script type="text/javascript">
+  function message(){
+    alert("您触发了选中事件！"); }
+</script>    
+</head>
+<body>
+  <form>
+  个人简介：<br>
+   <textarea name="summary" cols="60" rows="5" onselect="message(">请写入个人简介，不少于200字！</textarea>
+  </form>
+</body>
+</html>
