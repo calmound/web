3f0e9ab1aeb8eb38f8c5740b2cec3edd6cff9e7d
calmound <245708505@qq.com>
2015-11-12 18:38:36 +0800

    modify css js html

--- 前端/仿小米官网/js/js.js
+++ 前端/仿小米官网/js/js.js
@@ -47,6 +47,8 @@ $(function() {
 		}, 200);
 	});
 
+	//热门
+
 	//搜索框点击，移入，移出动画
 	$(document).click(function() {
 		$(".search_extra").hide();
@@ -96,6 +98,16 @@ $(function() {
 				break;
 		}
 	}
+
+	//首页大图切换
+	$(".category_move span").click(function(){
+		$(".category_move span").removeClass("cur_move");
+		$(this).addClass("cur_move");
+
+		var index = $(this).parent().index();
+		$(".category_hot_list > li").css("display","none");
+		$(".category_hot_list > li:eq("+index+")").css("display","block");
+	});
 
 	function changeStateUp(index) {
 		switch (index) {
@@ -178,16 +190,33 @@ $(function() {
 		}
 	});
 
+	//category栏的category_item_box显示隐藏
+	$(".category_item").hover(function(){
+		var index = $(this).index();
+		$(".category_item_box:eq("+index+")").css("display","block");
+			var category_item_list = $(this).find(".category_item_box").children(".category_item_list");
+			var width = $(this).index() == 0 ? 270 : 220;
+			var len = category_item_list.length;
+			category_item_list.width(width);
+			width = len == 1 ? len*width : len*width + 70;
+			$(".category_list .category_item_box").width(width);
+			console.log(len*width);
+	},function(){
+		var index = $(this).index();
+		$(".category_item_box:eq("+index+")").css("display","none");
+	})
+	
 	//为你推荐单品切换
-	var dir_recommend = 0; 
-	function changeDir(dir){
-		if(dir == 0){
+	var dir_recommend = 0;
+
+	function changeDir(dir) {
+		if (dir == 0) {
 			$(".img1 > img").attr("src", "images/icon/left_b_2.png");
 			$(".img2 > img").attr("src", "images/icon/right_b_1.png");
-		}else if(dir == 1){
+		} else if (dir == 1) {
 			$(".img1 > img").attr("src", "images/icon/left_b_1.png");
 			$(".img2 > img").attr("src", "images/icon/right_b_1.png");
-		}else{
+		} else {
 			$(".img1 > img").attr("src", "images/icon/left_b_1.png");
 			$(".img2 > img").attr("src", "images/icon/right_b_2.png");
 		}
@@ -199,19 +228,21 @@ $(function() {
 			$(".recommend .recommend_spec_list").animate({
 				left: "+=1234px"
 			}, 200);
-			
+
 		}
 	});
 	$(".img2").click(function() {
 		if (dir < 2) {
 			dir++;
-			dir = dir%3;
+			dir = dir % 3;
 			changeDir(dir);
 			$(".recommend .recommend_spec_list").animate({
 				left: "-=1234px"
 			}, 200);
 		}
 	});
+
+	// 单品热门导航切换
 	$(".match .spec_type_nav > li > a").hover(function() {
 		var index = $(this).index();
 		$(".match .cur").removeClass("cur");
@@ -226,6 +257,111 @@ $(function() {
 		var index = $(this).index();
 		$(".accessories .cur").removeClass("cur");
 		$(this).addClass("cur");
+	});
+
+
+
+	//内容单品切换
+	$(".content_spec > li").hover(function() {
+		var index = $(this).index();
+		var This = $(this);
+		$(this).find(".content_right,.content_left").css("display", "block");
+
+		//点击左移动
+		$(this).find(".content_left").hover(function() {
+			$(this).attr("src", "images/icon/content_left_hover.png");
+
+			var left = This.find(".content_spec_list").position().left;
+			if (left / -296 > 0) { //这里是移动进来的判断，除了第一个都需要变化鼠标
+				$(this).css("cursor", "pointer");
+			} else {
+				$(this).css("cursor", "default");
+			}
+
+			$(this).unbind('click').click(function() {
+				left = This.find(".content_spec_list").position().left;
+
+				if (!This.find(".content_spec_list").is(":animated") && left < 0) {
+					This.find(".content_spec_list").animate({
+						left: "+=296px"
+					});
+
+					//单品切换后，下面的圆圈也要跟着切换
+					This.find(".content_page > li > span").removeClass("active");
+					This.find(".content_page > li:eq(" + (left / -296 - 1) + ") > span").addClass("active");
+				}
+
+				//当点做第二个向左移动的时候，刚移动过去鼠标就要变化，所以是1
+				left = $(".content_spec_list").position().left;
+				if (left / -296 > 1) {
+					$(this).css("cursor", "pointer");
+				} else {
+					$(this).css("cursor", "default");
+				}
+
+				return false;
+			});
+
+		}, function() {
+			$(this).find(".content_left").attr("src", "images/icon/content_left.png");
+		});
+		//点击右移动
+		$(this).find(".content_right").hover(function() {
+
+			$(this).attr("src", "images/icon/content_right_hover.png");
+
+			var left = This.find(".content_spec_list").position().left;
+			if (left / -296 < 3) {
+				$(this).css("cursor", "pointer");
+			} else {
+				$(this).css("cursor", "default");
+			}
+
+			$(this).unbind('click').click(function() {
+				left = This.find(".content_spec_list").position().left;
+
+				if (!This.find(".content_spec_list").is(":animated") && left > -888) {
+					This.find(".content_spec_list").animate({
+						left: "-=296px"
+					});
+
+					//单品切换后，下面的圆圈也要跟着切换
+					This.find(".content_page > li > span").removeClass("active");
+					This.find(".content_page > li:eq(" + (left / -296 + 1) + ") > span").addClass("active");
+				}
+
+				//点击倒数第二个切换后，马上变化鼠标所以是2
+				if (left / -296 < 2) {
+					$(this).css("cursor", "pointer");
+				} else {
+					$(this).css("cursor", "default");
+				}
+				return false;
+			});
+		}, function() {
+			$(this).attr("src", "images/icon/content_right.png");
+			return false;
+		});
+		return false;
+	}, function() {
+		$(this).find(".content_right,.content_left").css("display", "none");
+	});
+
+	//内容content栏单品下面圆圈切换
+	$(".content_page > li > span").hover(function() {
+		$(this).click(function() {
+			var root = $(this).parent().parent().parent();
+			var This = $(this);
+			var index = $(this).parent().index();
+			var Left = -index * 296;
+			if (!root.find(".content_spec_list").is(":animated")) {
+				root.find(".content_page > li > span").removeClass("active");
+				This.addClass("active");
+				root.find(".content_spec_list").animate({
+					left: "" + Left + "px"
+				});
+			}
+		});
 	});
 
 
